5f3324efdfcd8d5f664dd99fe9134259ca8c224a
Henrique <ishenriquealbert@gmail.com>
2021-05-06 22:22:53 -0300

    :tada: Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f95f2690797881f4a5328bc0bc01a192'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '8096b7be-1b80-478d-a9d3-e5f9b44095ce'
+};
