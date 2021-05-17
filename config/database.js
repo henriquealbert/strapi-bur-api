7685d9605bb5c16685a15764ec9830c6563277c0
Henrique <ishenriquealbert@gmail.com>
2021-05-16 22:46:27 -0300

    :sparkles: Add Postgres

--- config/database.js
+++ config/database.js
@@ -4,12 +4,20 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        client: 'postgres',
+        host: env('DATABASE_HOST', 'localhost'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'burn_up_records'),
+        username: env('DATABASE_USERNAME', 'postgres'),
+        password: env('DATABASE_PASSWORD', 'postgres'),
+        schema: env('DATABASE_SCHEMA', 'public'), // Not Required
+        ssl: {
+          rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false) // For self-signed certificates
+        }
       },
       options: {
-        useNullAsDefault: true,
-      },
-    },
-  },
-});
+        ssl: env.bool('DATABASE_SSL', false)
+      }
+    }
+  }
+})
